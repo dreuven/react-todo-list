b51ec9969480518e5fe89da8cca6e56329fc56f0
Doron Reuven <dreuven13@berkeley.edu>
2017-01-09 12:15:06 -0800

    Part3 complete

--- client/client.js
+++ client/client.js
@@ -1,1 +1,9 @@
-
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
+// define the encompassing component that we want to mount,
+// DOM element we want to mount it to
+render(
+  <App/>,
+  document.getElementById('app')
+)
